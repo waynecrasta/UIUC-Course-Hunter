cbcbe43ccbfed0699d243fdd9f8d8877c03985c4
Wayne Crasta <wayne.crasta@gmail.com>
2016-04-07 23:20:04 -0500

    adding base file

---
+++ classchecker.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+import requests
+
+
+def check_open():
+    print "\n-- FALL 2016 Course Checker by Wayne --\n"
+    course = {'department':'ECE','number':408,'crn':58790}
+    # url = 'http://courses.illinois.edu/cisapp/explorer/schedule/2016/fall/{}/{}/{}.xml'.format(course['department'], course['number'], course['crn'])
+    url = 'http://courses.illinois.edu/cisapp/explorer/schedule/2016/fall/ECE/425/48121.xml'
+
+    r = requests.get(url)
+    xml = r.text
+    try:
+        root = ET.fromstring(xml)
+    except:
+        print "Class doesn't exist :("
+        return
+    avail = root.find('enrollmentStatus').text
+    if avail == "CrossListOpen" or avail == "Open":
+        return 1
+    elif avail == "CrossListOpen (Restricted)":
+        return 1
+    else:
+        return 0
+
+if __name__ == '__main__':
+    if check_open():
+        print "OPEN"
+    else:
+        print "CLOSED"
+    raw_input("\nPress any key to exit...")
